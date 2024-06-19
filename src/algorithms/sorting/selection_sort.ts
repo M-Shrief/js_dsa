79c803bd1e3e3fafceabf1b85521543daec7cf79
M-Shrief <m.shrief.dev@proton.me>
2024-06-19 21:49:02 +0300

    Added Selection Sort.

---
+++ src/algorithms/sorting/selection_sort.ts
@@ -0,0 +1,21 @@
+import { DIRECTION, swap } from './utils';
+
+// Time Big O: O(n^2) || Space Big O: O(1)  
+export function selectionSort(list: number[], directon: DIRECTION): number[] {
+    for(let i = 0; i < list.length - 1; i++) {
+        for(let j = i+1; j < list.length; j++) {
+
+            if(directon == DIRECTION.ASC) {
+                if(list[i] > list[j]) {
+                    swap(list, i, j);
+                }
+            } else {
+                if(list[i] < list[j]) {
+                    swap(list, i,j);
+                }
+            }
+
+        }
+    }
+    return list;
+}
