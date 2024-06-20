b50e0f3d5083a9def241148fd7c054d382685f76
M-Shrief <m.shrief.dev@proton.me>
2024-06-20 19:12:07 +0300

    Added Stack

---
+++ src/ds/stack/stack.spec.ts
@@ -0,0 +1,27 @@
+import {describe, test, expect} from 'vitest'
+import {Stack} from './stack'
+
+describe.concurrent("Testing Stack", async () => {
+    test("Testing Push()", async() => {
+        let s = new Stack<number>();
+        s.push(1);
+        expect(s.getTop()).toEqual(1);
+        s.push(2);
+        expect(s.getTop()).toEqual(2);
+        s.push(3);
+        expect(s.getTop()).toEqual(3);
+
+        expect(s.getSize()).toEqual(3)
+    })
+
+    test("Testing Pop()", async() => {
+        let s = new Stack<number>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        expect(s.pop()).toEqual(3);
+        expect(s.pop()).toEqual(2);
+        expect(s.getSize()).toEqual(1);
+    })
+})
---
+++ src/ds/stack/stack.ts
@@ -0,0 +1,31 @@
+import { SignlyLinkedList } from "../linkedlist/singly";
+
+export class Stack<T>{
+    private list: SignlyLinkedList<T>;
+    private size: number;
+
+    constructor() {
+        this.list = new SignlyLinkedList<T>();
+        this.size = this.list.getSize();
+    }
+
+    public getTop(): T | null {
+        let top =  this.list.getHead()
+        if(!top) return null;
+        return top.data;
+    }
+
+    public getSize(): number {
+        return this.list.getSize()
+    }
+
+    public push(data: T) {
+        return this.list.addFirst(data);       
+    }
+    
+    public pop(): T | null {
+        return this.list.deleteFirst()
+    }
+
+
+}
