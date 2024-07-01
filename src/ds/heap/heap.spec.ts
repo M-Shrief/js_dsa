7805d91d2ca2a51943bae643183328ce48b6b637
M-Shrief <m.shrief.dev@proton.me>
2024-07-01 08:31:08 +0300

    Improving Heap.delete() tests

--- src/ds/heap/heap.spec.ts
+++ src/ds/heap/heap.spec.ts
@@ -46,13 +46,13 @@ describe.concurrent("Testing Heap", async() => {
             h.push(6);
             h.push(4);    
 
-            h.delete(1);
+            expect(h.delete(1)).toEqual(true);
             expect(h.getTop()).toEqual(4)
 
             // Delete non-existing value without errors
-            h.delete(5)
-            h.delete(100)
-            h.delete(-1)
+            expect(h.delete(5)).toEqual(false)
+            expect(h.delete(100)).toEqual(false)
+            expect(h.delete(-1)).toEqual(false)
         })
     })
 
@@ -92,6 +92,21 @@ describe.concurrent("Testing Heap", async() => {
             expect(h.getTop()).toEqual(-1);
             h.pop()
             expect(h.getTop()).toEqual(-3);
+        })
+
+        test("Testing Delete()", async() => {
+            let h = new Heap<number>(isBigger);
+            h.push(1);
+            h.push(6);
+            h.push(4);    
+
+            expect(h.delete(6)).toEqual(true);
+            expect(h.getTop()).toEqual(4)
+
+            // Delete non-existing value without errors
+            expect(h.delete(5)).toEqual(false)
+            expect(h.delete(100)).toEqual(false)
+            expect(h.delete(-1)).toEqual(false)
         })
     })
 })
