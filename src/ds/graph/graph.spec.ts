65f409358672f78363d5e403220c22f97cd20879
M-Shrief <m.shrief.dev@proton.me>
2024-07-08 11:15:51 +0300

    Updated test to be more resembling.

--- src/ds/graph/graph.spec.ts
+++ src/ds/graph/graph.spec.ts
@@ -12,10 +12,13 @@ describe.concurrent('Testing Graph', async () => {
     g.addEdge(2, 0);
     g.addEdge(2, 3);
     g.addEdge(3, 3);
+    g.addEdge(3, 4);
+    g.addEdge(5, 4);
 
-    expect(g.BFT(0)).toEqual([0, 1, 2, 3]);
-    expect(g.BFT(1)).toEqual([1, 2, 0, 3]);
-    expect(g.BFT(2)).toEqual([2, 0, 3, 1]);
+    expect(g.BFT(0)).toEqual([0, 1, 2, 3, 4]);
+    expect(g.BFT(1)).toEqual([1, 2, 0, 3, 4]);
+    expect(g.BFT(2)).toEqual([2, 0, 3, 1, 4]);
+    expect(g.BFT(4)).toEqual([4]);
   });
 
   test('Testing DFT()', async () => {
@@ -24,14 +27,16 @@ describe.concurrent('Testing Graph', async () => {
     
     g.addEdge(0, 1);
     g.addEdge(0, 2);
-    g.addEdge(1, 2);
+    g.addEdge(1, 3);
     g.addEdge(2, 0);
     g.addEdge(2, 3);
     g.addEdge(3, 3);
+    g.addEdge(3, 4);
+    g.addEdge(4, 5);
 
-    expect(g.DFT(0)).toEqual([0, 1, 2, 3]);
-    expect(g.DFT(1)).toEqual([1, 2, 0, 3]);
-    expect(g.DFT(2)).toEqual([2, 0, 1, 3 ]);
-    expect(g.DFT(3)).toEqual([3]);
+    expect(g.DFT(0)).toEqual([0, 1, 3, 4, 5, 2]);
+    expect(g.DFT(1)).toEqual([1, 3, 4, 5]);
+    expect(g.DFT(2)).toEqual([2, 0, 1, 3, 4, 5]);
+    expect(g.DFT(3)).toEqual([3, 4, 5]);
   });
 });
